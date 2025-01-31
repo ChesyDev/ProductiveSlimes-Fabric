6b4591bbe8d01cb9652d3813b6304c10a89cfb62
CoolerProRB <118531502+CoolerProRB@users.noreply.github.com>
2025-01-31 13:16:26 +0800

    Small fixes

--- src/main/java/com/chesy/productiveslimes/event/ModServerLifecycleEvent.java
+++ src/main/java/com/chesy/productiveslimes/event/ModServerLifecycleEvent.java
@@ -1,24 +1,22 @@
 package com.chesy.productiveslimes.event;
 
 import com.chesy.productiveslimes.config.CustomVariantRegistry;
-import com.chesy.productiveslimes.network.ModNetworkState;
 import com.chesy.productiveslimes.network.ModNetworkStateManager;
 import com.chesy.productiveslimes.network.RecipeSyncPayload;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.recipe.RecipeManager;
 import net.minecraft.recipe.ServerRecipeManager;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 
 public class ModServerLifecycleEvent {
     public static void init() {
-        ServerLifecycleEvents.SERVER_STARTING.register(CustomVariantRegistry::handleDatapack);
+        ServerLifecycleEvents.SERVER_STARTING.register(minecraftServer -> {
+            CustomVariantRegistry.handleDatapack(minecraftServer);
+            minecraftServer.getCommandManager().execute(minecraftServer.getCommandManager().getDispatcher().parse("reload", minecraftServer.getCommandSource()), "reload");
+        });
 
         ServerLifecycleEvents.SERVER_STOPPING.register(minecraftServer -> {
             ServerWorld overworld = minecraftServer.getOverworld();
@@ -34,7 +32,6 @@ public class ModServerLifecycleEvent {
         PayloadTypeRegistry.playS2C().register(RecipeSyncPayload.TYPE, RecipeSyncPayload.STREAM_CODEC);
 
         ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-            server.getCommandManager().execute(server.getCommandManager().getDispatcher().parse("reload", server.getCommandSource()), "reload");
             ServerRecipeManager recipeManager = server.getRecipeManager();
 
             ServerPlayNetworking.send(handler.player, new RecipeSyncPayload(recipeManager.values().stream().toList()));
