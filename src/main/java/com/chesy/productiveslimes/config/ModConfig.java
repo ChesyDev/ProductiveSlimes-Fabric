ca0a4534d6475a97cb2561f734215621c14cd108
CoolerProYT <veronlam1818@gmail.com>
2025-02-21 15:39:06 +0800

    v1.4.11

---
+++ src/main/java/com/chesy/productiveslimes/config/ModConfig.java
@@ -0,0 +1,31 @@
+package com.chesy.productiveslimes.config;
+
+import com.chesy.productiveslimes.ProductiveSlimes;
+import com.electronwill.nightconfig.core.file.FileConfig;
+
+public class ModConfig {
+    public static void config(){
+        ProductiveSlimes.config = FileConfig.builder(ProductiveSlimes.CONFIG_PATH.toFile())
+                .autosave()
+                .build();
+
+        if (!ProductiveSlimes.config.getFile().exists()) {
+            ProductiveSlimes.config.save();
+        }
+
+        ProductiveSlimes.config.load();
+
+        if (!ProductiveSlimes.config.contains("slime_settings.vanilla_slime_can_attack_player")) {
+            ProductiveSlimes.config.add("slime_settings.vanilla_slime_can_attack_player", false);
+        }
+
+        if (!ProductiveSlimes.config.contains("iron_golem_settings.iron_golem_can_attack_slime")) {
+            ProductiveSlimes.config.add("iron_golem_settings.iron_golem_can_attack_slime", false);
+        }
+
+        ProductiveSlimes.config.save();
+
+        ProductiveSlimes.vanillaSlimeCanAttackPlayer = ProductiveSlimes.config.get("slime_settings.vanilla_slime_can_attack_player");
+        ProductiveSlimes.ironGolemCanAttackSlime = ProductiveSlimes.config.get("iron_golem_settings.iron_golem_can_attack_slime");
+    }
+}
--- src/main/java/com/chesy/productiveslimes/worldgen/ModPlacedFeatures.java
+++ src/main/java/com/chesy/productiveslimes/worldgen/ModPlacedFeatures.java
@@ -8,21 +8,38 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.feature.PlacedFeatures;
 import net.minecraft.world.gen.feature.VegetationPlacedFeatures;
+import net.minecraft.world.gen.placementmodifier.HeightmapPlacementModifier;
 import net.minecraft.world.gen.placementmodifier.PlacementModifier;
+import net.minecraft.world.gen.placementmodifier.RarityFilterPlacementModifier;
+import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 
 import java.util.List;
 
 public class ModPlacedFeatures {
     public static RegistryKey<PlacedFeature> SLIMY_TREE = registerKey("slimy_tree");
+    public static RegistryKey<PlacedFeature> LAKE_MOLTEN_DIRT = registerKey("lake_molten_dirt");
+    public static RegistryKey<PlacedFeature> LAKE_MOLTEN_STONE = registerKey("lake_molten_stone");
 
     public static void bootstrap(Registerable<PlacedFeature> context) {
         RegistryEntryLookup<ConfiguredFeature<?, ?>> configuredFeatureGetter = context.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
 
         register(context, ModPlacedFeatures.SLIMY_TREE, configuredFeatureGetter.getOrThrow(ModConfiguredFeatures.SLIMY_TREE), VegetationPlacedFeatures.treeModifiersWithWouldSurvive(PlacedFeatures.createCountExtraModifier(1, 0.005f, 1), ModBlocks.SLIMY_SAPLING));
+
+        register(context, ModPlacedFeatures.LAKE_MOLTEN_DIRT, configuredFeatureGetter.getOrThrow(ModConfiguredFeatures.LAKE_MOLTEN_DIRT), List.of(
+                RarityFilterPlacementModifier.of(200),
+                SquarePlacementModifier.of(),
+                HeightmapPlacementModifier.of(Heightmap.Type.WORLD_SURFACE_WG)
+        ));
+        register(context, ModPlacedFeatures.LAKE_MOLTEN_STONE, configuredFeatureGetter.getOrThrow(ModConfiguredFeatures.LAKE_MOLTEN_STONE), List.of(
+                RarityFilterPlacementModifier.of(250),
+                SquarePlacementModifier.of(),
+                HeightmapPlacementModifier.of(Heightmap.Type.WORLD_SURFACE_WG)
+        ));
     }
 
     private static RegistryKey<PlacedFeature> registerKey(String name) {
